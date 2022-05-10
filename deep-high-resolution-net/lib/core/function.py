f22484eba3ea2a1eb9af0e8131d88812042e44d5
AlyaNovikova <aleksnovikova@gmail.com>
2022-05-10 20:20:33 +0600

    fixes

--- deep-high-resolution-net/lib/core/function.py
+++ deep-high-resolution-net/lib/core/function.py
@@ -176,7 +176,7 @@ def validate(config, val_loader, val_dataset, model, criterion, output_dir,
     idx = 0
     with torch.no_grad():
         end = time.time()
-        for i, (input, target, target_weight, meta, _) in enumerate(val_loader):
+        for i, (input, target, target_weight, meta) in enumerate(val_loader):
             # compute output
             outputs, _ = model(input)
             if isinstance(outputs, list):
